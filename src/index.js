7e86753d05204344ba765f60403ef0e5a164d2b4
MoBakri <mohamedbinbakri@gmail.com>
2021-09-24 15:00:16 +0200

    init quote app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import App from "./freeCodeCamp/quotes";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
-import "./index.css";
+// import "./index.css";
+import "./quotes.css";
 
 ReactDOM.render(
   <BrowserRouter>
